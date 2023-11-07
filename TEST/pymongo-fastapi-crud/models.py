49564ccec300a73a4d898e4bd1eb2d4719c487e0
Alarcon <alarcozm@amazon.com>
2023-11-07 17:19:43 -0500

    add Book model with required fields

--- TEST/pymongo-fastapi-crud/models.py
+++ TEST/pymongo-fastapi-crud/models.py
@@ -1,3 +1,23 @@
 import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
+
+#model for creating a book entry
+#all entries are required since it is creating an entry for the first time
+class Book(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    title: str = Field(...)
+    author: str = Field(...)
+    synopsis: str = Field(...)
+
+    #what is this for?
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "title": "Don Quixote",
+                "author": "Miguel de Cervantes",
+                "synopsis": "..."
+            }
+        }
