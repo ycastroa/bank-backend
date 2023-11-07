22dd90c87a8b7eb3da1e33c739244e81557f9136
Alarcon <alarcozm@amazon.com>
2023-11-07 16:05:10 -0500

    add updated lifespan handling via asynccontextmanager for startup and shutdown of MongoDB client

--- TEST/pymongo-fastapi-crud/main.py
+++ TEST/pymongo-fastapi-crud/main.py
@@ -8,6 +8,16 @@ config = dotenv_values(".env")
 
 app = FastAPI()
 
+#setup lifespan for startup and shutdown
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+    #shutdown
+    app.mongodb_client.close()
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the PyMongo tutorial!"}
